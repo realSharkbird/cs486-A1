7362fe200c680cd45166557932800ff4cd7b5097
DESKTOP-A233PBB\Jayson <sharcbird@gmail.com>
2017-10-01 15:44:12 -0400

    progress

--- src/Sudoku/Sudoku.java
+++ src/Sudoku/Sudoku.java
@@ -5,53 +5,100 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.TreeSet;
 
 public class Sudoku {
 
 	public static void main(String[] args) throws IOException {
 
-		int[][] grid = new int[9][9];
-		int[][] closed = new int[9][9];
+		TreeSet<Integer> x = new TreeSet<Integer>(); //keeps all the num of initial values in sorted order
+		HashMap<Integer, Integer> sum = new HashMap<Integer, Integer>(); //keeps the overall sum 
+		HashMap<Integer, Integer> num = new HashMap<Integer, Integer>(); //keeps the number of times that num of init values occurred
 
-		//read grid here
-		BufferedReader reader = new BufferedReader(new FileReader(new File("C:\\Users\\Jayson\\workspace\\cs486_A1\\src\\problems\\71\\10.sd")));
+		int startFolder = 1;
+		int startInstance = 1;
+		int endFolder = 72;
+		int endInstance = 11;
 
-		for(int i = 0; i < grid.length; i++) {
-			String line = reader.readLine();
-			String[] row = line.split(" ");
-			String[] closedRow = line.replaceAll("[1-9]", "-1").split(" ");
-			closed[i] = Arrays.asList(closedRow).stream().mapToInt(Integer::parseInt).toArray();
-			grid[i] = Arrays.asList(row).stream().mapToInt(Integer::parseInt).toArray();
-		}
+		for(int folder = startFolder; folder < endFolder; folder++) {
+			for(int instance = startInstance; instance < endInstance; instance++) {
 
-		reader.close();
+				System.out.println("folder: " + folder + " | instance: " + instance);
+				int[][] grid = new int[9][9];
+				int[][] closed = new int[9][9];
+				int numInitial = 0;
+				
+				//read grid here
+				BufferedReader reader = new BufferedReader(new FileReader(new File("C:\\Users\\Jayson\\workspace\\cs486_A1\\src\\problems\\" + folder + "\\" + instance + ".sd")));
 
-		//print grid here
-		System.out.println("BEFORE: ");
-		for(int i = 0; i < grid.length; i++) {
-			for(int k = 0; k < grid[i].length; k++) {
-				System.out.print(grid[i][k] + " ");
+				for(int i = 0; i < grid.length; i++) {
+					String line = reader.readLine();
+					String[] row = line.split(" ");
+					String[] closedRow = line.replaceAll("[1-9]", "-1").split(" ");
+					closed[i] = Arrays.asList(closedRow).stream().mapToInt(Integer::parseInt).toArray();
+					grid[i] = Arrays.asList(row).stream().mapToInt(Integer::parseInt).toArray();
+				}
+
+				reader.close();
+
+				for(int i = 0; i < closed.length; i++) {
+					for(int k = 0; k < closed.length; k++) {
+						if(closed[i][k] == -1)
+							numInitial++;
+					}
+				}
+				
+				x.add(numInitial);
+				
+				//print grid here
+				/*System.out.println("Num of initial values: " + numInitial);
+				for(int i = 0; i < grid.length; i++) {
+					for(int k = 0; k < grid[i].length; k++) {
+						System.out.print(grid[i][k] + " ");
+					}
+					System.out.println("");
+				}*/
+
+				Agent a = new Agent();
+				if(!VersionA(grid, closed, a))
+					System.out.println("Error. There were likely more than 10,000 variable assignments.");
+
+				//print grid here
+				/*System.out.println("");
+				System.out.println("SOLUTION: ");
+				for(int i = 0; i < grid.length; i++) {
+					for(int k = 0; k < grid[i].length; k++) {
+						System.out.print(grid[i][k] + " ");
+					}
+					System.out.println("");
+				}*/
+				
+				Integer currentSum, currentNum = num.get(numInitial);
+				if((currentSum = sum.get(numInitial)) == null) {
+					currentSum = 0;
+					currentNum = 0;
+				}
+				sum.put(numInitial, currentSum + a.var_assign);
+				num.put(numInitial, currentNum + 1);
+				
+				System.out.println("variable assignments: " + a.var_assign);
+				System.out.println("");
+
 			}
-			System.out.println("");
 		}
+		
+		while(!x.isEmpty()) {
+			Integer numVarAssign = x.first();
+			x.remove(numVarAssign);
+			double avg = sum.get(numVarAssign) / (double)num.get(numVarAssign);
+			System.out.println("Num of initial values: " + numVarAssign);
+			System.out.println("Average number of var assignments: " + avg);
 
-		if(!VersionA(grid, closed))
-			System.out.println("Error.");
-
-		//print grid here
-		System.out.println("");
-		System.out.println("AFTER: ");
-		for(int i = 0; i < grid.length; i++) {
-			for(int k = 0; k < grid[i].length; k++) {
-				System.out.print(grid[i][k] + " ");
-			}
-			System.out.println("");
 		}
 	}
 
-	public static boolean VersionA(int[][] grid, int[][] closed) {
-
-		Agent a = new Agent();
+	public static boolean VersionA(int[][] grid, int[][] closed, Agent a) {
 
 		while(a.r < grid.length) {
 			while(a.c < grid[a.r].length) {
@@ -63,14 +110,18 @@ public class Sudoku {
 				}
 
 				//choose next possible number from constrained list
-				if(nextValue(grid, a) == 0) {
+				if(nextValueA(grid, a) == 0) {
 					//if no number exists, backtrack
-					if(!backtrack(closed, grid, a)) {
+					if(!backtrackA(closed, grid, a)) {
 						//No solution found
 						return false;
 					}
 				}else {
 					a.c ++;
+				}
+
+				if(a.var_assign > 10000) {
+					return false;
 				}
 			}
 
@@ -80,15 +131,60 @@ public class Sudoku {
 		return true;
 
 	}
-	
-	public static boolean VersionB(int[][] grid, int[][] closed) {
-		
+
+
+	public static boolean VersionB(int[][] grid, int[][] closed, Agent a) {
+
+		int[][][] forwardCheck = new int[9][9][9];
+		for(int i = 0; i < forwardCheck.length; i++) {
+			for(int k = 0; k < forwardCheck[i].length; k++) {
+				for(int j = 0; j < forwardCheck[i][k].length; j++) {
+					forwardCheck[i][k][j] = 0;
+				}
+			}
+		}
+
+		for(int r = 0; r < 9; r++) {
+			for(int c = 0; c < 9; c++) {
+				forwardCheck(forwardCheck, grid, r, c, 1);
+			}
+		}
+
+		while(a.r < grid.length) {
+			while(a.c < grid[a.r].length) {
+
+				//find next empty position (in orderly fashion)
+				if(closed[a.r][a.c] == -1) {
+					a.c ++;
+					continue;
+				}
+
+				//choose next possible number from constrained list
+				if(nextValueB(forwardCheck, grid, a) == 0) {
+					//if no number exists, backtrack
+					if(!backtrackB(forwardCheck, closed, grid, a)) {
+						//No solution found
+						return false;
+					}
+				}else {
+					a.c ++;
+				}
+
+				if(a.var_assign > 10000) {
+					return false;
+				}
+			}
+
+			a.r ++; a.c = 0;
+		}
+
 		return true;
 	}
 
 	//generates the next possible value for a cell given the set constraints
 	//if no value is found, return 0
-	public static int nextValue(int[][] grid, Agent a) {
+	public static int nextValueA(int[][] grid, Agent a) {
+
 		int r = a.r;
 		int c = a.c;
 		int currentValue = grid[r][c];
@@ -97,7 +193,7 @@ public class Sudoku {
 		constraintCheck:
 			for(int i = currentValue + 1; i <= 9; i++) {
 				//check if this value is possible
-
+				
 				//constraint 1: check horizontal cells
 				for(int k = 0; k < grid[r].length; k++) {
 					if(grid[r][k] == i) {
@@ -125,6 +221,7 @@ public class Sudoku {
 				}
 
 				//all constraints passed. This value is acceptable atm.
+				a.var_assign ++;
 				nextValue = i;
 				break;
 
@@ -134,8 +231,79 @@ public class Sudoku {
 		return nextValue;
 	}
 
-	public static boolean backtrack(int[][] closed, int[][] grid, Agent a) {
+	public static boolean backtrackA(int[][] closed, int[][] grid, Agent a) {
+		return backtrackB(null, closed, grid, a);
+	}
 
+	public static void resetCell(int[][][] forwardCheck, int[][] grid, Agent a) {
+		int r = a.r; int c = a.c;
+		if(forwardCheck != null) {
+			forwardCheck(forwardCheck, grid, r, c, -1);
+		}
+		grid[r][c] = 0;
+		a.var_assign++;
+	}
+
+	public static void stepback(Agent a) {
+		a.c--;
+		if(a.c < 0) {
+			a.r --;
+			a.c = 8;
+		}
+	}
+
+	public static int nextValueB(int[][][] forwardCheck, int[][] grid, Agent a) {
+
+		int r = a.r;
+		int c = a.c;
+		int currentValue = grid[r][c];
+		int nextValue = 0;
+
+		forwardCheck(forwardCheck, grid, r, c, -1);
+
+		for(int j = currentValue; j < forwardCheck[r][c].length; j++) {
+			int valid = forwardCheck[r][c][j];
+			if(valid == 0) {
+				nextValue = j + 1;
+				grid[r][c] = nextValue;
+				a.var_assign++;
+				forwardCheck(forwardCheck, grid, r, c, 1);
+				break;
+			}
+		}
+
+		grid[r][c] = nextValue;
+		a.var_assign++;
+		return nextValue;
+
+	}
+
+	public static void forwardCheck(int[][][] forwardCheck, int[][] grid, int r, int c, int increment){
+		int var = grid[r][c] - 1;
+
+		if(var < 0) return;
+
+		//check row
+		for(int i = 0; i < 9; i++) {
+			forwardCheck[i][c][var] += increment;
+		}
+
+		//check column
+		for(int i = 0; i < 9; i++) {
+			forwardCheck[r][i][var] += increment;
+		}
+
+		//check block
+		int sR = (int) Math.floor(r/3.0);
+		int sC = (int) Math.floor(c/3.0);
+		for(int k = sR * 3; k < (sR + 1) * 3; k++) {
+			for(int j = sC * 3; j < (sC + 1) * 3; j++) {
+				forwardCheck[k][j][var] += increment;
+			}
+		}
+	}
+
+	public static boolean backtrackB(int[][][] forwardCheck, int[][] closed, int[][] grid, Agent a) {
 		stepback(a); //step back from current cell
 
 		while(a.r >= 0) {
@@ -148,11 +316,11 @@ public class Sudoku {
 					break;
 				}
 				if(grid[r][c] == 9) {
-					grid[r][c] = 0; //reset cell
+					resetCell(forwardCheck, grid, a);
 					stepback(a); //cell has no more options, keep going back
 					break;
 				}
-				
+
 				//an empty cell is found
 				return true;
 
@@ -162,22 +330,16 @@ public class Sudoku {
 		return false;
 	}
 
-	public static void stepback(Agent a) {
-		a.c--;
-		if(a.c < 0) {
-			a.r --;
-			a.c = 8;
-		}
-	}
-
 	//Represents the cell you are currently checking
 	public static class Agent {
 		public Integer r, c;
+		public int var_assign = 0;
 
 		public Agent() {
 			r = 0;
 			c = 0;
 		}
+		
 	}
 
 }
--- src/TSP/TSP.java
+++ src/TSP/TSP.java
@@ -11,34 +11,56 @@ public class TSP {
 
 	public static void main(String[] args) throws NumberFormatException, IOException{
 
-		int numCities;
-		ArrayList<City> cities = new ArrayList<City>();
-		SearchNode result = null;
-		
-		//read nodes here
-		BufferedReader reader = new BufferedReader(new FileReader(new File("C:\\Users\\Jayson\\workspace\\cs486_A1\\src\\randTSP\\16\\instance_1.txt")));
-		numCities = Integer.parseInt(reader.readLine());
-		for(int i = 0; i < numCities; i++) {
-			String line = reader.readLine();
-			String[] words = line.split(" ");
-			String letter = words[0];
-			int x = Integer.parseInt(words[1]);
-			int y = Integer.parseInt(words[2]);
-			City c = new City(letter, x, y);
-			cities.add(c);
-		}
-		reader.close();
-		
-		//do A* search here
-		if((result = SolveTSP(cities)) == null) {
-			System.out.println("error");
-		}else {
-			//print results here
-			while(result != null) {
-				System.out.println(result.city.getLetter());
-				result = result.cameFrom;
+		int startFolder = 1;
+		int startInstance = 1;
+		int endFolder = 17;
+		int endInstance = 11;
+
+		for(int folder = startFolder; folder < endFolder; folder++) {
+			for(int instance = startInstance; instance < endInstance; instance++) {
+
+				int numCities;
+				ArrayList<City> cities = new ArrayList<City>();
+				SearchNode result = null;
+
+				//read nodes here
+				BufferedReader reader = new BufferedReader(new FileReader(new File("C:\\Users\\Jayson\\workspace\\cs486_A1\\src\\randTSP\\" + folder + "\\instance_" + instance + ".txt")));
+				numCities = Integer.parseInt(reader.readLine());
+				for(int i = 0; i < numCities; i++) {
+					String line = reader.readLine();
+					String[] words = line.split(" ");
+					String letter = words[0];
+					int x = Integer.parseInt(words[1]);
+					int y = Integer.parseInt(words[2]);
+					City c = new City(letter, x, y);
+					cities.add(c);
+				}
+				reader.close();
+
+				System.out.println("CITIES: ");
+				for(int i = 0; i < cities.size(); i++) {
+					System.out.print(cities.get(i).getLetter() + " ");
+				}
+				System.out.println();
+
+				//do A* search here
+				if((result = SolveTSP(cities)) == null) {
+					System.out.println("error");
+				}else {
+					//print results here
+					System.out.println();
+
+					System.out.println("SOLUTION: ");
+					SearchNode resultBackup = result;
+					while(result != null) {
+						System.out.print(result.city.getLetter() + " ");
+						result = result.cameFrom;
+					}
+					System.out.println();
+					System.out.println("Total Distance: " + resultBackup.gScore);
+
+				}
 			}
-			
 		}
 	}
 
@@ -47,7 +69,7 @@ public class TSP {
 	public static SearchNode SolveTSP(ArrayList<City> cities){
 
 		City start = cities.get(0);
-		
+
 		//represents all the current paths being visited in the search tree
 		TreeSet<SearchNode> open = new TreeSet<SearchNode>(new Comparator<SearchNode>() {
 
@@ -65,9 +87,9 @@ public class TSP {
 
 		SearchNode searchNode = new SearchNode(start, cities);
 		searchNode.gScore = 0;
-		searchNode.fScore = getHeuristic(start, start);
+		searchNode.fScore = getHeuristic(searchNode.open_cities);
 		open.add(searchNode);
-		
+
 		while(!open.isEmpty()){
 
 			SearchNode node = open.first();
@@ -75,26 +97,27 @@ public class TSP {
 
 			open.remove(node);
 			node.visit(city);
-			
+
 			if(node.visited.size() == cities.size()) {
-				
+
 				return node;
 
 			}
-			
+
 			for(City neighbour: node.open_cities) {
 
 				SearchNode child = new SearchNode(neighbour, node);
-				
+
+				//this is here so we end where we started
 				if(!child.city.equals(start) && child.visited.size() == cities.size() - 1) {
 					child.unVisit(start);
 				}
-				
+
 				double tentative_gScore = node.gScore + getCost(city, neighbour);
 
 				child.cameFrom = node;
 				child.gScore = tentative_gScore;
-				child.fScore = child.gScore + getHeuristic(neighbour, start);
+				child.fScore = child.gScore + getHeuristic(child.open_cities);
 
 				open.add(child);
 			}
@@ -104,12 +127,104 @@ public class TSP {
 		return null;
 	}
 
-
-
 	//heuristic function
 	//which generates the min span tree
-	public static double getHeuristic(City n1, City n2) {
-		return 0;
+	public static double getHeuristic(ArrayList<City> open_cities) {
+
+		ArrayList<ArrayList<City>> forest = new ArrayList<ArrayList<City>>(); //keep track of disjointed nodes
+		ArrayList<Edge> MST = new ArrayList<Edge>();
+
+		//kruskals algorithm
+		//create list of edges
+		TreeSet<Edge> edges = new TreeSet<Edge>(new Comparator<Edge>() {
+
+			@Override
+			public int compare(Edge e1, Edge e2) {
+
+				//keep list sorted by length
+				double dist1 = getCost(e1.c1, e1.c2);
+				double dist2 = getCost(e2.c1, e2.c2);
+
+				if(dist1 < dist2) return -1;
+				if(dist1 > dist2) return 1;
+
+				return 0;
+			}
+
+		});
+
+		for(int i = 0; i < open_cities.size(); i++) {
+			for(int k = i + 1; k < open_cities.size(); k++) {
+				Edge e = new Edge(open_cities.get(i), open_cities.get(k));
+				edges.add(e);
+			}
+		}
+
+
+		while(!edges.isEmpty()) {
+
+			Edge shortest = edges.first(); 	//pick the shortest edge to check first
+			edges.remove(shortest);
+
+			int merge = -1;
+
+			for(int i = 0; i < forest.size(); i++) {
+				ArrayList<City> tree = forest.get(i);
+
+				//there is a cycle, don't add this edge to the MST
+				if(tree.contains(shortest.c1) && tree.contains(shortest.c2)) {
+					merge = -2;
+					break;
+				}
+
+				City merger = null;
+				if(tree.contains(shortest.c1)) {
+					merger = shortest.c1;
+					tree.add(shortest.c2);
+				}
+
+				if(tree.contains(shortest.c2)) {				
+					merger = shortest.c2;
+					tree.add(shortest.c1);
+				}
+
+				//
+				if(merger != null) {
+					if(merge != -1) {
+
+						//merge 2 disjointed sets together
+						ArrayList<City> previous = forest.get(merge);
+						previous.addAll(tree);
+						previous.remove(shortest.c1);
+						previous.remove(shortest.c2);
+						forest.remove(tree);
+						break;
+
+					}else{
+
+						MST.add(shortest);
+						merge = i;
+					}
+				}
+
+			}
+
+			//if edge does not connect to any current edges, create a new disjointed set
+			if(merge == -1) {
+				ArrayList<City> tree = new ArrayList<City>();
+				tree.add(shortest.c1);
+				tree.add(shortest.c2);
+				forest.add(tree);
+				MST.add(shortest);
+			}
+		}
+
+		double heuristic = 0;
+		for(int i = 0; i < MST.size(); i++) {
+			heuristic += MST.get(i).distance;
+		}
+
+		return heuristic;
 	}
 
 	//real cost function
@@ -125,25 +240,25 @@ public class TSP {
 		public double fScore = Double.POSITIVE_INFINITY, gScore = Double.POSITIVE_INFINITY; //f and g score of this path
 		public SearchNode cameFrom; //to rebuild the path if solution is found
 
+
 		//self-explanatory
 		public ArrayList<City> visited = new ArrayList<City>();
 		public ArrayList<City> open_cities = new ArrayList<City>();
-		
+
 		public SearchNode(City n, ArrayList<City> open_cities) {
 			this.city = n;
-			
+
 			//this is pretty much the sucessor function
 			this.open_cities.addAll(open_cities);
 		}
-		
+
 		public SearchNode(City n, SearchNode searchNode) {
 			this.city = n;
-			
-			//this is pretty much the sucessor function
+
 			this.open_cities.addAll(searchNode.open_cities);
 			this.visited.addAll(searchNode.visited);
 		}
-		
+
 		public void visit(City c) {
 			visited.add(c);
 			open_cities.remove(c);
@@ -153,7 +268,21 @@ public class TSP {
 			visited.remove(c);
 			open_cities.add(c);
 		}
-		
+
+	}
+
+	public static class Edge {
+
+		public City c1, c2;
+		public double distance = 0;
+
+		public Edge(City c1, City c2) {
+			this.c1 = c1;
+			this.c2 = c2;
+
+			distance = getCost(c1, c2);
+		}
+
 	}
 
 }
